485c754659d31b8d8aac6db098540d47a277711c
Robin Shindelman <shindelr@flip1.engr.oregonstate.edu>
2024-02-13 19:24:43 -0800

    Got the db_connector hooked up, though I put it in the .gitignore since my password is in there an stuff! Got a nav bar across all pages (i think). Got Jets to dynamically show a heading.

--- app.js
+++ app.js
@@ -4,19 +4,31 @@
 // Server setup 
 var express = require('express');
 var app = express()
-PORT = 5000
+PORT = 5001
 // Template (handlebars) setup
 const {engine} = require('express-handlebars');
 var exphbs = require('express-handlebars');
 app.engine('.hbs', engine({extname: ".hbs"}));
 app.set('view engine', '.hbs');
+// Tell express where to find static files
+app.use(express.static('public'));
+// Database connection
+var db = require('./database/db_connector')
 
 
 // ROUTES
-app.get('/', function(req, res)
-    {
-        res.render('index');  // Renders the index.hbs template
-    
+// Display Home page
+app.get('/index', function(req, res){
+        res.render('index');  // Renders the index.hbs template   
+});
+
+// Browse Jets
+app.get('/jets', function(req, res){
+    let query1 = "select * from Jets;";
+    db.pool.query(query1, function(error, rows, fields){
+        console.log(rows)
+        res.render('jets', {data: rows});
+    })
 });
 
 
