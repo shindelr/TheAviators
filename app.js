e38239910dfd95420a686353300a2544346f8e29
Alma Valenzuela <valenalm@flip2.engr.oregonstate.edu>
2024-02-25 14:09:58 -0800

    Added POST function for Airports table

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var db = require('./database/db_connector')
 
 
 // ROUTES
+// GET Functions
 // Display Home page
 app.get('/', function(req, res){
         res.render('index');  // Renders the index.hbs template   
@@ -101,6 +102,54 @@ app.get('/routes', function(req, res){
         res.render('routes', {data: rows});       
     })   
 });
+
+//POST Functions
+
+app.post('/add-airport', function(req, res) 
+{
+    let data = req.body;
+    // Capture NULL values - State can be NULL
+    let state = parseInt(data.state);
+    if (isNaN(state))
+    {state = 'NULL'}
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Airports (airport_id, city, state, country) VALUES ('${data.code}', '${data.city}', ${data.state}, ${data.country})`;
+    db.pool.query(query1, function(error, rows, fields){
+        // Check to see if there was an error
+        if (error) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `select airport_id as Airport_Code, city as City, 
+            state as State, country as Country 
+            from Airports;`;
+            db.pool.query(query2, function(error, rows, fields){
+                // If there was an error on the second query, send a 400
+                if (error) {               
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {res.send(rows);}
+            })
+        }
+    })
+});
+
+
+
+
+
+
+
+
+
 
 
 // LISTENER
