e8c4b2c1279167f5d99cff431f2166b93eb31b7e
Alma Valenzuela <97207314+valenalm@users.noreply.github.com>
2024-02-27 19:46:54 -0600

    Create delete_route.js and added code to delete selected route

---
+++ public/js/delete_route.js
@@ -0,0 +1,28 @@
+//The following code is based on the CS340 nodejs-starter-app
+
+function deleteRoute(routeID) {
+  let link = '/delete-route/';
+  let data = {
+    id: routeID
+  };
+
+  $.ajax({
+    url: link,
+    type: 'DELETE',
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    success: function(result) {
+      deleteRow(routeID);
+    }
+  });
+}
+
+function deleteRow(routeID){
+    let table = document.getElementById("routes-table");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+       if (table.rows[i].getAttribute("data-value") == routeID) {
+            table.deleteRow(i);
+            break;
+       }
+    }
+}
